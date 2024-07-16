5189139ddecb48ee2b9e4d2761ecf80d11e30c09
akrucancom <andrzej.krucan@stazysta.comarch.pl>
2024-07-16 14:43:10 +0200

    HWS_27-29 Challenge

--- HWS-27-29/HWS-27-29/ViewController.swift
+++ HWS-27-29/HWS-27-29/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UITableViewController {
 		super.viewDidLoad()
 		
 		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(restartGame))
 
 		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
 			if let startWords = try? String(contentsOf: startWordsURL) {
@@ -51,12 +52,25 @@ class ViewController: UITableViewController {
 		
 		let submitAction = UIAlertAction(title: "Submit", style: .default) {
 			[weak self, weak ac] _ in
-			guard let answer = ac?.textFields?[0].text else { return }
+			guard let answer = ac?.textFields?[0].text?.lowercased() else { return }
 			self?.submit(answer)
 		}
 		
 		ac.addAction(submitAction)
 		present(ac, animated: true)
+	}
+	
+	@objc func restartGame() {
+		let restartGameAC = UIAlertController(title: "Restart game?", message: "New title word will be generated and answers will be wiped", preferredStyle: .alert)
+		let restartAA = UIAlertAction(title: "Proceed", style: .destructive) { _ in
+			self.startGame()
+		}
+		let closeAA = UIAlertAction(title: "Close", style: .default) { _ in
+		}
+		
+		restartGameAC.addAction(restartAA)
+		restartGameAC.addAction(closeAA)
+		present(restartGameAC, animated: true)
 	}
 	
 	func submit(_ answer: String) {
@@ -76,20 +90,18 @@ class ViewController: UITableViewController {
 				} else {
 					errorTitle = "Word not real"
 					errorMessage = "Eloquent lika a 5 year old, are we?"
+					showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
 				}
 			} else {
 				errorTitle = "Word was used already"
 				errorMessage = "Basic bozo"
+				showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
 			}
 		} else {
 			errorTitle = "Word not possible"
 			errorMessage = "Where do you see letters you used in \(title!.lowercased())"
+			showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
 		}
-		
-		let errorAC = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-		let errorAction = UIAlertAction(title: "OK", style: .default)
-		errorAC.addAction(errorAction)
-		present(errorAC, animated: true)
 	}
 	
 	func isPossible(word: String) -> Bool {
@@ -102,8 +114,11 @@ class ViewController: UITableViewController {
 				return false
 			}
 		}
-		
-		return true
+		if word.count < 4 || word == tempWord {
+			return false
+		} else {
+			return true
+		}
 	}
 	
 	func isoriginal(word: String) -> Bool {
@@ -115,5 +130,12 @@ class ViewController: UITableViewController {
 		let range = NSRange(location: 0, length: word.utf16.count)
 		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
 		return misspelledRange.location == NSNotFound
+	}
+	
+	func showErrorMessage(errorTitle: String, errorMessage: String) {
+		let errorAC = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+		let errorAction = UIAlertAction(title: "OK", style: .default)
+		errorAC.addAction(errorAction)
+		present(errorAC, animated: true)
 	}
 }
