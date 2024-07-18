40b84be8df5e64cabc8dc795e373d9ff5d07bb14
akrucancom <andrzej.krucan@stazysta.comarch.pl>
2024-07-18 14:48:09 +0200

    HWS-40-challenges-setup

--- HWS-40-Project-2/HWS-33-35/AppDelegate.swift
+++ HWS-40-Project-2/HWS-33-35/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  HWS-Project1
+//  HWS-33-35
 //
-//  Created by Comarch-Andrzej on 11/07/2024.
+//  Created by Comarch-Andrzej on 17/07/2024.
 //
 
 import UIKit
@@ -10,10 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
 		return true
 	}
 
@@ -30,7 +27,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 	}
-
-
 }
-
