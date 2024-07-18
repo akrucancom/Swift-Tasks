bbc0dff1bd3b1f68783cb1bb40462af39c53b248
akrucancom <andrzej.krucan@stazysta.comarch.pl>
2024-07-18 14:58:18 +0200

    HWS-40 challenge setup

---
+++ HWS-40-Project-3/HWS-33-35/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  HWS-33-35
+//
+//  Created by Comarch-Andrzej on 17/07/2024.
+//
+
+import Foundation
+
+struct Petition: Codable {
+	var title: String
+	var body: String
+	var signatureCount: Int
+}
