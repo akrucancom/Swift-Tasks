c86e933fae0403d29854768cc68efdc6eed80076
akrucancom <andrzej.krucan@stazysta.comarch.pl>
2024-07-19 14:10:19 +0200

    HWS_40 Challenge completed

---
+++ HWS-40-Challenge 3/HWS-Project1/ImageCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCell.swift
+//  HWS-Project1
+//
+//  Created by Comarch-Andrzej on 19/07/2024.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+	@IBOutlet var imageView: UIImageView!
+}
