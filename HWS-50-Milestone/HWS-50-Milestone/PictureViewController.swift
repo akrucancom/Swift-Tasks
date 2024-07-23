5411f0c10176c85291d0e71d9fdd0a2057c363a6
akrucancom <andrzej.krucan@stazysta.comarch.pl>
2024-07-23 13:30:29 +0200

    HWS_50 milestone completed

---
+++ HWS-50-Milestone/HWS-50-Milestone/PictureViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PictureViewController.swift
+//  HWS-50-Milestone
+//
+//  Created by Comarch-Andrzej on 23/07/2024.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+	var picture: Picture?
+	@IBOutlet var photoImageView: UIImageView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		title = picture?.caption
+		print(picture!.name, picture!.caption)
+		guard let picture else { return }
+		let picturePath = getDocumentsDirectory().appendingPathComponent(picture.name)
+		photoImageView.image = UIImage(contentsOfFile: picturePath.path)
+    }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		navigationController?.hidesBarsOnTap = true
+	}
+
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		navigationController?.hidesBarsOnTap = false
+	}
+	
+	func getDocumentsDirectory() -> URL {
+		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+		return paths[0]
+	}
+
+}
